8efe24267d2f53a9a2b698fdbd02e46a56410fc7
dusk <52311860+MC-dusk@users.noreply.github.com>
2024-11-15 14:47:07 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
-import * as React from "react"
+import React from "react"
 
-export default function Home() {
-  return <h1>Goodbye world!</h1>
-}
+export default () => (
+  <div style={{ color: `purple` }}>
+    <a href="https://www.youtube.com/watch?v=I1bIti1SihE"><h1>Hot Drop Mode Anthem 1-Hour Looping</h1></a>
+    <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=113462667640878&bvid=BV1dBm2YJEJ6&cid=26709658093&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+  </div>
+)
