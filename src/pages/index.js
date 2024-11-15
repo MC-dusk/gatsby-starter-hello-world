ffffefb24347fac172469b102bccfb7baab01b75
dusk <52311860+MC-dusk@users.noreply.github.com>
2024-11-15 15:18:11 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ export default () => (
   <div style={{ color: `purple` }}>
     <h1>Hot Drop Mode Anthem 1-Hour Looping</h1>
     <p><a href="https://www.youtube.com/watch?v=I1bIti1SihE">Jump now</a></p>
-    <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=113462667640878&bvid=BV1dBm2YJEJ6&cid=26709658093&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" style={{width: fit-content, height: fit-content}}></iframe>
+    <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=113462667640878&bvid=BV1dBm2YJEJ6&cid=26709658093&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="100%" height="100%"></iframe>
   </div>
 )
